22609eb2d54a8fe53eeb3f506b0ec712bd347f88
cesc1 <francesc.aguirre@gmail.com>
2025-11-27 03:04:02 +0100

    add minimal map reader
    add mini map reader (import_tmp_map(path)). Use path to load a simple map to the input structure. Texture paths and colors need to be loaded the manually (check main)

--- include/cub3d.h
+++ include/cub3d.h
@@ -1,6 +1,39 @@
 #ifndef CUB3D_H
-#define CUB3D_H
+# define CUB3D_H
 
-#include "stddef.h"
+# include "stddef.h"
+
+// Enum types
+
+typedef enum e_return_status
+{
+	OK,
+	ERR_MALLOC,
+	ERR_OPEN,
+	ERR_PARSER
+}	t_return_status;
+
+// Struct types
+
+typedef struct s_input
+{
+	char			**map;
+	char			*texture_n;
+	char			*texture_s;
+	char			*texture_e;
+	char			*texture_o;
+	unsigned int	col_c;
+	unsigned int	col_f;
+}	t_input;
+
+// Functions import
+char	**import_tmp_map(const char *path);
+void	print_map(const t_input *input);
+void	free_map(char **map);
+int		is_char_map(const char c);
+int		is_line_map(const char *line);
+
+// Functions error
+void	print_error(t_return_status type);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,7 +1,17 @@
-#include "libft.h"
+#include "cub3d.h"
 
-int main(void)
+int	main(void)
 {
-    ft_printf("Hello world\n");
-    return (0);
+	t_input	input;
+
+	input.map = import_tmp_map("data/fakemap.cub");
+	input.texture_n = "...";
+	input.texture_s = "...";
+	input.texture_e = "...";
+	input.texture_o = "...";
+	input.col_c = 0x00000000;
+	input.col_f = 0x00000000;
+	print_map(&input);
+	free_map(input.map);
+	return (0);
 }
