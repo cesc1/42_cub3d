dd4b93759b6d6f9292b34e55143eb5aa57227e28
Gyan Polo <gpolo@car7s5.42barcelona.com>
2026-01-20 11:33:01 +0100

    [fix]: Change norm spaces to tabs

--- include/cub3d.h
+++ include/cub3d.h
@@ -11,46 +11,46 @@
 typedef enum e_return_status
 {
 	OK,
-    ERR,
+	ERR,
 	ERR_MALLOC,
 	ERR_OPEN
-}					t_return_status;
+}				t_return_status;
 
 // Estructura para inputs
 typedef struct s_input
 {
-	char			**map;
-	char			*texture_n;
-	char			*texture_s;
-	char			*texture_e;
-	char			*texture_o;
-	ssize_t	        col_c;
-	ssize_t     	col_f;
-	int				w;
-	int				h;
-    int             start_row;
-    int             start_col;
-    char            start_dir;
-}					t_input;
+	char		**map;
+	char		*texture_n;
+	char		*texture_s;
+	char		*texture_e;
+	char		*texture_o;
+	ssize_t		col_c;
+	ssize_t		col_f;
+	int			w;
+	int			h;
+	int			start_row;
+	int			start_col;
+	char		start_dir;
+}				t_input;
 
 // Estructura para file_reader
 typedef struct s_file_data
 {
-    char     **data;
-    size_t  size;
-    size_t  capacity;
-    size_t  max_width;
-}   t_file_data;
+	char		**data;
+	size_t		size;
+	size_t		capacity;
+	size_t		max_width;
+}				t_file_data;
 
 // Img_data // estructruara para acelerar la impresion de pixeles
 typedef struct s_img_data
 {
-	void			*img;
-	char			*addr;
-	int				bits_per_pixel;
-	int				line_length;
-	int				endian;
-}					t_img_data;
+	void		*img;
+	char		*addr;
+	int			bits_per_pixel;
+	int			line_length;
+	int			endian;
+}				t_img_data;
 
 // Vision // estructura para los valores de visión
 
@@ -68,78 +68,76 @@ typedef struct s_img_data
 
 typedef struct s_vision
 {
-	double			pos[2];
-	double			dir[2];
-	double			plane[2];
-	double			rayDir[2];
-	double			sideDist[2];
-	double			deltaDist[2];
-	double			perpWallDist;
-	double			camera;
-	int				step[2];
-	int				map[2];
-	int				draw[2];
-	int				side;
-	int				lineHeight;
-}					t_vision;
+	double		pos[2];
+	double		dir[2];
+	double		plane[2];
+	double		rayDir[2];
+	double		sideDist[2];
+	double		deltaDist[2];
+	double		perpWallDist;
+	double		camera;
+	int			step[2];
+	int			map[2];
+	int			draw[2];
+	int			side;
+	int			lineHeight;
+}				t_vision;
 
 // Mlx_vars // estructrura necesaria para el correcto funcionamiento de hook
 typedef struct s_mlx_vars
 {
-	void			*mlx;
-	void			*win;
-	t_img_data		img;
-	t_vision		vision;
-	t_input			input;
-	int				screen[2];
-}					t_mlx_vars;
+	void		*mlx;
+	void		*win;
+	t_img_data	img;
+	t_vision	vision;
+	t_input		input;
+	int			screen[2];
+}				t_mlx_vars;
 
 // Functions import
-int                 check_args(int argc);
-void                init_input(t_input *input);
-void                free_input(t_input *input);
-int                 parser(int argc, char **argv, t_input *input);
-int                 save_texture(char *line, char **ptr_save, int line_num);
-int                 save_color(char *line, ssize_t *ptr_save, int line_num);
-int                 create_map(t_input *input, char *line, int fd);
-void                input_print(t_input *input);
-int                 file_data_init(t_file_data *file);
-int                 file_data_push(t_file_data *file, char *line);
-void                file_data_free(t_file_data *file);
+int				check_args(int argc);
+void			init_input(t_input *input);
+void			free_input(t_input *input);
+int				parser(int argc, char **argv, t_input *input);
+int				save_texture(char *line, char **ptr_save, int line_num);
+int				save_color(char *line, ssize_t *ptr_save, int line_num);
+int				create_map(t_input *input, char *line, int fd);
+void			input_print(t_input *input);
+int				file_data_init(t_file_data *file);
+int				file_data_push(t_file_data *file, char *line);
+void			file_data_free(t_file_data *file);
 
 // Functions error
-void				print_error(t_return_status type);
+void			print_error(t_return_status type);
 
 // Funcions TRGB // Utils/trgb.c
-int					create_trgb(int t, int r, int g, int b);
-int					get_t(int trgb);
-int					get_r(int trgb);
-int					get_g(int trgb);
-int					get_b(int trgb);
+int				create_trgb(int t, int r, int g, int b);
+int				get_t(int trgb);
+int				get_r(int trgb);
+int				get_g(int trgb);
+int				get_b(int trgb);
 
 // temp Funcion to test // Utils/draw.c
-void				draw_filled_circle(t_img_data *data, int xc, int yc, int r,
-						int color);
-void				draw_cros(t_img_data *data, int x_size, int y_size,
-						int color);
-void				draw_vison(t_img_data *data, double *pos, double *v_vis,
-						int color);
-void				clear_image(t_img_data *data, int width, int height,
-						int color);
-void				draw_full_vision(t_img_data *data, t_vision *vision,
-						int *screen, t_input imput);
-void				init_vision(t_vision *vision, char **map);
+void			draw_filled_circle(t_img_data *data, int xc, int yc, int r,
+					int color);
+void			draw_cros(t_img_data *data, int x_size, int y_size, int color);
+void			draw_vison(t_img_data *data, double *pos, double *v_vis,
+					int color);
+void			clear_image(t_img_data *data, int width, int height, int color);
+void			draw_full_vision(t_img_data *data, t_vision *vision,
+					int *screen, t_input imput);
+void			init_vision(t_vision *vision, char **map);
 
 // Funcion i crete to render mlx // Utils/my_mlx_func.c
-void				my_mlx_pixel_put(t_img_data *data, int x, int y, int color);
+void			my_mlx_pixel_put(t_img_data *data, int x, int y, int color);
 
 // Funcion close_win // Utils/close_mlx.c
-int					close_mlx(t_mlx_vars *vars);
+int				close_mlx(t_mlx_vars *vars);
 
 // Funcion open_win // Utils/open.mlx.c
-int					open_win(t_mlx_vars *data, int x, int y, char *name);
+int				open_win(t_mlx_vars *data, int x, int y, char *name);
 
 // Funcions to handel the input // Logic/handle_input.c
-int					handle_input(int keysym, t_mlx_vars *data);
+int				handle_input(int keysym, t_mlx_vars *data);
 
 #endif
--- src/Parser/map.c
+++ src/Parser/map.c
@@ -1,128 +1,125 @@
 #include "cub3d.h"
-
+#include <libft.h>
 #include <stdlib.h>
 
-#include <libft.h>
-
-static int  check_start_pos(t_input *input, int r, int c)
+static int	check_start_pos(t_input *input, int r, int c)
 {
-    if (!ft_strchr("NSEW", input->map[r][c]))
-        return (OK);
-    if (input->start_dir)
-        return (ft_printf("Error\n.cub: Map double start position\n"), ERR);
-    input->start_dir = input->map[r][c];
-    input->start_row = r;
-    input->start_col = c;
-    return (OK);
+	if (!ft_strchr("NSEW", input->map[r][c]))
+		return (OK);
+	if (input->start_dir)
+		return (ft_printf("Error\n.cub: Map double start position\n"), ERR);
+	input->start_dir = input->map[r][c];
+	input->start_row = r;
+	input->start_col = c;
+	return (OK);
 }
 
-static int  copy_map(t_file_data *raw_map, t_input *input)
+static int	copy_map(t_file_data *raw_map, t_input *input)
 {
-    int c;
-    int r;
-    int flag_endl;
+	int	c;
+	int	r;
+	int	flag_endl;
 
-    r = -1;
-    while (++r < input->h)
-    {
-        c = -1;
-        flag_endl = 0;
-        while (++c < input->w)
-        {
-            if (!flag_endl && (!raw_map->data[r][c] || raw_map->data[r][c] == '\n'))
-                flag_endl = 1;
-            if (flag_endl)
-                input->map[r][c] = ' ';
-            else
-            {
-                input->map[r][c] = raw_map->data[r][c];
-                if (check_start_pos(input, r, c) != OK)
-                    return (ERR);
-            }
-        }
-    }
-    return (OK);
+	r = -1;
+	while (++r < input->h)
+	{
+		c = -1;
+		flag_endl = 0;
+		while (++c < input->w)
+		{
+			if (!flag_endl && (!raw_map->data[r][c]
+					|| raw_map->data[r][c] == '\n'))
+				flag_endl = 1;
+			if (flag_endl)
+				input->map[r][c] = ' ';
+			else
+			{
+				input->map[r][c] = raw_map->data[r][c];
+				if (check_start_pos(input, r, c) != OK)
+					return (ERR);
+			}
+		}
+	}
+	return (OK);
 }
 
-static int  save_memory_map(t_input *input)
+static int	save_memory_map(t_input *input)
 {
-    int i;
+	int	i;
 
-    input->map = ft_calloc(input->h, sizeof (char *));
-    if (!input->map)
-        return (ft_printf("Error\n.cub: Malloc error\n"), ERR_MALLOC);
-
-    i = -1;
-    while (++i < input->h)
-    {
-        input->map[i] = ft_calloc(input->w, sizeof (char));
-        if (!input->map[i])
-            return (ft_printf("Error\n.cub: Malloc error\n"), ERR_MALLOC);
-    }
-    return (OK);
+	input->map = ft_calloc(input->h, sizeof(char *));
+	if (!input->map)
+		return (ft_printf("Error\n.cub: Malloc error\n"), ERR_MALLOC);
+	i = -1;
+	while (++i < input->h)
+	{
+		input->map[i] = ft_calloc(input->w, sizeof(char));
+		if (!input->map[i])
+			return (ft_printf("Error\n.cub: Malloc error\n"), ERR_MALLOC);
+	}
+	return (OK);
 }
 
-static int  check_line(t_file_data *raw_map, char *line)
+static int	check_line(t_file_data *raw_map, char *line)
 {
-    int i;
+	int	i;
 
-    i = -1;
-    while (line[++i] && line[i] != '\n')
-    {
-        if (!ft_strchr(" 01NSEW", line[i]))
-            return (ERR);    
-    }
-    while (i > 0 && line[i - 1] == ' ')
-        i--;
-    if (i > (int)raw_map->max_width)
-        raw_map->max_width = i;
-    return (OK);
+	i = -1;
+	while (line[++i] && line[i] != '\n')
+	{
+		if (!ft_strchr(" 01NSEW", line[i]))
+			return (ERR);
+	}
+	while (i > 0 && line[i - 1] == ' ')
+		i--;
+	if (i > (int)raw_map->max_width)
+		raw_map->max_width = i;
+	return (OK);
 }
 
-static int read_raw_map(t_file_data *raw_map, char *line, int fd)
+static int	read_raw_map(t_file_data *raw_map, char *line, int fd)
 {
-    int flag_eof;
+	int	flag_eof;
 
-    if (file_data_init(raw_map) != OK)
-        return (ERR_MALLOC);
-    flag_eof = 0;
-    line = ft_strdup(line);
-    if (!line)
-        return (ft_printf("Error\n.cub: Malloc error\n"), ERR_MALLOC);
-    while (line)
-    {
-        if (line[0] == '\n')
-            flag_eof = 1;
-        else
-        {
-            if (flag_eof == 1 || \
-                file_data_push(raw_map, line) != OK || \
-                check_line(raw_map, line) != OK)
-            {
-                file_data_free(raw_map);
-                return (ft_printf("Error\n.cub: Map error (map l%d)\n", raw_map->size), ERR);
-            }
-        }
-        line = get_next_line(fd);
-    }
-    return (OK);
+	if (file_data_init(raw_map) != OK)
+		return (ERR_MALLOC);
+	flag_eof = 0;
+	line = ft_strdup(line);
+	if (!line)
+		return (ft_printf("Error\n.cub: Malloc error\n"), ERR_MALLOC);
+	while (line)
+	{
+		if (line[0] == '\n')
+			flag_eof = 1;
+		else
+		{
+			if (flag_eof == 1 || file_data_push(raw_map, line) != OK
+				|| check_line(raw_map, line) != OK)
+			{
+				file_data_free(raw_map);
+				return (ft_printf("Error\n.cub: Map error (map l%d)\n",
+						raw_map->size), ERR);
+			}
+		}
+		line = get_next_line(fd);
+	}
+	return (OK);
 }
 
-int create_map(t_input *input, char *line, int fd)
+int	create_map(t_input *input, char *line, int fd)
 {
-    t_file_data raw_map;
+	t_file_data raw_map;
 
-    if (read_raw_map(&raw_map, line, fd) != OK)
-        return (ERR);
-    input->h = raw_map.size;
-    input->w = raw_map.max_width;
-    if (save_memory_map(input) != OK || \
-        copy_map(&raw_map, input) != OK)
-    {
-        free_input(input);
-        file_data_free(&raw_map);
-        return (ERR);
-    }
-    file_data_free(&raw_map);
-    return (OK);
+	if (read_raw_map(&raw_map, line, fd) != OK)
+		return (ERR);
+	input->h = raw_map.size;
+	input->w = raw_map.max_width;
+	if (save_memory_map(input) != OK || copy_map(&raw_map, input) != OK)
+	{
+		free_input(input);
+		file_data_free(&raw_map);
+		return (ERR);
+	}
+	file_data_free(&raw_map);
+	return (OK);
 }
